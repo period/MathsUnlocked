64238bd5fd488b6ce36809e4ffafe1ec907f100d
Thomas <chearful@chearfulcraft.org>
2020-06-19 13:40:37 +0100

    Create navigation bar

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+  
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand to="/">Maths Unlocked</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-button to="/login">Login</b-button>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
