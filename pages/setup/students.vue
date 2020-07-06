61f193d5a37d6582371a9adcca836cb994c926fd
Thomas <chearful@chearfulcraft.org>
2020-07-06 11:17:57 +0100

    Continue work on setup

---
+++ pages/setup/students.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h1 class="title">
+          Welcome to Maths Unlocked! Let's get your school setup.
+      </h1>
+    </div>
+    <b-row>
+        <b-col lg="3" />
+        <b-col lg="6">
+            <b-card
+                header="Student Setup"
+                header-tag="header"
+            >
+            <b-card-body>
+                <b-form-select v-model="importMethod">
+                    <b-form-select-option value="null" disabled>How would you like to create students?</b-form-select-option>
+                    <b-form-select-option value="manual">Manually</b-form-select-option>
+                    <b-form-select-option value="csv">CSV Import</b-form-select-option>
+                </b-form-select>
+                <div v-if="importMethod == 'manual'">
+                    <strong>Enter the student's details below:</strong>
+                    <b-input-group class="mt-3">
+                        <b-form-input id="name" placeholder="First & Last Name" type="text" v-model="name" />
+                    </b-input-group>
+                    <b-input-group class="mt-3">
+                        <b-form-input id="email" placeholder="Email address" type="email" v-model="email" />
+                    </b-input-group>
+                </div>
+                <div v-if="importMethod == 'csv'">
+                    <strong>Select a CSV file:</strong>
+                    <p>The CSV file must be comma separated with no headings. Each student should be delimeted by a newline and each column should be delimeted by a comma with no quotes.</p>
+                    <p>Format: <code>student full name,student email</code></p>
+                    <p>Usernames and passwords will be generated at random</p>
+                    <b-form-file accept=".csv" v-model="csvFile" :state="Boolean(csvFile)" placeholder="Select a file" drop-placeholder="Drag and drop a file" />
+                    <p>{{ importStatus }}</p>
+                </div>
+                <div class="mt-3">
+                    <b-btn-group>
+                        <b-button v-if="importMethod != null"variant="primary" :disabled="disableCreateBtn()" @click="create()"><span v-if="importMethod == 'manual'">Create student</span><span v-else :disabled="this.csvFile == null" @click="csvImport()">Import students</span></b-button>
+                        <b-button variant="secondary" @click="$nuxt.$router.push('/setup/students')">Next Step</b-button>
+                    </b-btn-group>
+                </div>
+            </b-card-body>
+            </b-card>
+        </b-col>
+        <b-col lg="3" />
+    </b-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "SetupTeachers",
+    components: {
+    },
+    data() {
+        return {
+            importMethod: null,
+            csvFile: null,
+            name: null,
+            username: null,
+            password: null,
+            email: null,
+            activeRequest: false,
+            importStatus: "Waiting for a file to be selected...",
+            studentsToImport: []
+        }
+    },
+    methods: {
+        async csvImport() {
+            let reader = new FileReader();
+            reader.onload = e => {
+                this.studentsToImport = [];
+                let lines = e.target.result.split(/\r?\n/);
+                for(var i = 0; i < lines.length; i++) {
+                    if(lines[i].includes(",") == false) continue;
+                    let data = lines[i].split(",");
+                    if(data[1].includes("@") == false || data[1].includes(".") == false) continue;
+                    if(data[0].includes(" ") == false) continue;
+                    this.studentsToImport.push({name: data[0], email: data[1]})
+                }
+                this.importStatus = "Found " + this.studentsToImport.length + " students for import. Please verify that the data is correct before importing.";
+            }       
+            reader.readAsText(this.csvFile);
+        },
+        async create() {
+            this.activeRequest = true;
+            await this.$axios.$put("https://mathsunlockedapi.thomas.gg/school/" + localStorage.getItem("schoolID") + "/teachers", [{name: this.name, username: this.username, email: this.email, password: this.password}], {
+                headers: {}
+            })
+            .then((res) => {
+                this.activeRequest = false;
+                this.name = null;
+                this.email = null;
+                this.username = null;
+                this.password = null;
+                this.$toastr(
+                    "success",
+                    "Teacher account created successfully",
+                    "Creation successful"
+                );
+            })
+            .catch((e) => {
+                this.activeRequest = false;
+            })
+        },
+        disableCreateBtn() {
+            if(this.name == null || this.name.length < 5 || this.name.trim().includes(" ") == false) return true;
+            if(this.email == null || this.email.length < 5 || this.email.includes("@") == false || this.email.includes(".") == false) return true;
+            if(this.username == null || this.username.length < 3) return true;
+            if(this.password == null || this.password.length < 3) return true;
+            if(this.activeRequest == true) return true;
+            return false;
+        }
+    }
+}
+</script>
--- pages/setup/teachers.vue
+++ pages/setup/teachers.vue
@@ -9,16 +9,11 @@
         <b-col lg="3" />
         <b-col lg="6">
             <b-card
-                header="School Setup"
+                header="Teacher Setup"
                 header-tag="header"
             >
             <b-card-body>
-                <strong>What is your school called?</strong>
-                <b-input-group>
-                    <b-form-input id="school" placeholder="School name" type="text" v-model="school" />
-                </b-input-group>
-                <hr>
-                <strong>Create a teacher account to administrate this school:</strong>
+                <strong>Enter the teacher's details below:</strong>
                 <b-input-group class="mt-3">
                     <b-form-input id="name" placeholder="First & Last Name" type="text" v-model="name" />
                 </b-input-group>
@@ -31,9 +26,13 @@
                 <b-input-group class="mt-3">
                     <b-form-input id="password" placeholder="Password" type="password" v-model="password" />
                 </b-input-group>
-                <small>This account will be used to manage teachers, students and their classes. Don't worry, you'll be able to transfer ownership of the school later.</small>
-                <div class="mt-3"><b-button variant="primary" :disabled="disableCreateBtn()" @click="create()">Create school</b-button></div>
-                <div class="mt-2"><n-link to="/login">Already have an account?</n-link></div>
+                <small>Teachers have access to setting work for students as well as the capability of resetting a student's password. If you don't want to create any teachers right now, just click on 'Next Step'</small>
+                <div class="mt-3">
+                    <b-btn-group>
+                        <b-button variant="primary" :disabled="disableCreateBtn()" @click="create()">Create teacher</b-button>
+                        <b-button variant="secondary" @click="$nuxt.$router.push('/setup/students')">Next Step</b-button>
+                    </b-btn-group>
+                </div>
             </b-card-body>
             </b-card>
         </b-col>
@@ -45,12 +44,11 @@
 <script>
 
 export default {
-    name: "SetupSchool",
+    name: "SetupTeachers",
     components: {
     },
     data() {
         return {
-            school: null,
             name: null,
             username: null,
             password: null,
@@ -61,26 +59,26 @@ export default {
     methods: {
         async create() {
             this.activeRequest = true;
-            await this.$axios.$put("https://mathsunlockedapi.thomas.gg/school", {name: this.school, administrator: {name: this.name, username: this.username, email: this.email, password: this.password}}, {
+            await this.$axios.$put("https://mathsunlockedapi.thomas.gg/school/" + localStorage.getItem("schoolID") + "/teachers", {name: this.name, username: this.username, email: this.email, password: this.password}, {
                 headers: {}
             })
             .then((res) => {
                 this.activeRequest = false;
-                localStorage.setItem("authorization", res.token);
-                localStorage.setItem("schoolID", res.school.id);
+                this.name = null;
+                this.email = null;
+                this.username = null;
+                this.password = null;
                 this.$toastr(
                     "success",
-                    "You will be redirected momentarily to the next step",
+                    "Teacher account created successfully",
                     "Creation successful"
                 );
-                $nuxt.$router.push('/setup/teachers')
             })
             .catch((e) => {
                 this.activeRequest = false;
             })
         },
         disableCreateBtn() {
-            if(this.school == null || this.school.length < 3) return true;
             if(this.name == null || this.name.length < 5 || this.name.trim().includes(" ") == false) return true;
             if(this.email == null || this.email.length < 5 || this.email.includes("@") == false || this.email.includes(".") == false) return true;
             if(this.username == null || this.username.length < 3) return true;
