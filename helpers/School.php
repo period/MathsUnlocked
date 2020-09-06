5a366f7207b5c84c50daced7756430b42f0e0231
Thomas <chearful@chearfulcraft.org>
2020-09-06 13:56:26 +0100

    Push to latest api

---
+++ helpers/School.php
@@ -0,0 +1,61 @@
+<?php
+    class School {
+        private $id = null;
+        private $name = null;
+        private $owner = null;
+
+        public function __construct($id, $data=array()) {
+            $this->id = $id;
+            foreach($data as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+
+        public function export() {
+            return ["id" => $this->getId(), "owner" => $this->getOwner(), "name" => $this->getName()];
+        }
+        
+        public function load($conn) {
+            $stmt = $conn->prepare("SELECT name, owner FROM schools WHERE id = ?");
+            $stmt->bind_param("i", $this->id);
+            $stmt->execute();
+            $stmt->bind_result($this->name, $this->owner);
+            $stmt->fetch();
+            $stmt->close();
+            return true;
+        }
+        public function save($conn) {
+            $stmt = $conn->prepare("UPDATE schools SET name = ?, owner = ? WHERE id = ?;");
+            $stmt->bind_param("sis", $this->name, $this->owner, $this->id);
+            $stmt->execute();
+            $stmt->close();
+            return true;
+        }
+        public function create($conn) {
+            $stmt = $conn->prepare("INSERT INTO schools VALUES (null, ?, ?);");
+            $stmt->bind_param("si",  $this->name, $this->owner);
+            $res = $stmt->execute();
+            $this->id = $stmt->insert_id;
+            $stmt->close();
+            return $res;
+        }
+
+        public function getId() {
+            return $this->id;
+        }
+        public function setId($id) {
+            $this->id = $id;
+        }
+        public function getName() {
+            return $this->name;
+        }
+        public function setName($name) {
+            $this->name = $name;
+        }
+        public function getOwner() {
+            return $this->owner;
+        }
+        public function setOwner($owner) {
+            $this->owner = $owner;
+        }
+    }
---
+++ helpers/Student.php
@@ -0,0 +1,82 @@
+<?php
+    class Student {
+        private $id = null;
+        private $school = null;
+        private $username = null;
+        private $email = null;
+        private $hash = null;
+        private $name = null;
+
+        public function __construct($uid, $data=array()) {
+            $this->id = $uid;
+            foreach($data as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+
+        public function export() {
+            return ["id" => $this->getId(), "username" => $this->getUsername(), "email" => $this->getEmail(), "school" => $this->getSchool(), "name" => $this->getName()];
+        }
+
+        public function load($conn) {
+            $stmt = $conn->prepare("SELECT id, school, username, email, hash, name FROM students WHERE id = ?;");
+            $stmt->bind_param("s", $this->id);
+            $stmt->execute();
+            $stmt->bind_result($this->id, $this->school, $this->username, $this->email, $this->hash, $this->name);
+            $stmt->fetch();
+            $stmt->close();
+            return true;
+        }
+        public function save($conn) {
+            $stmt = $conn->prepare("UPDATE students SET school = ?, username = ?, email = ?, hash = ?, name = ? WHERE id = ?;");
+            $stmt->bind_param("issssi", $this->school, $this->username, $this->email, $this->hash, $this->name, $this->id);
+            $stmt->execute();
+            $stmt->close();
+            return true;
+        }
+        public function create($conn) {
+            $stmt = $conn->prepare("INSERT INTO students VALUES (null, ?, ?, ?, ?, ?);");
+            $stmt->bind_param("issss", $this->school, $this->username, $this->email, $this->hash, $this->name);
+            $res = $stmt->execute();
+            $this->id = $stmt->insert_id;
+            $stmt->close();
+            return $res;
+        }
+
+        public function getId() {
+            return $this->id;
+        }
+        public function setId($id) {
+            $this->id = $id;
+        }
+        public function getSchool() {
+            return $this->school;
+        }
+        public function setSchool($school) {
+            $this->school = $school;
+        }
+        public function getUsername() {
+            return $this->username;
+        }
+        public function setUsername($username) {
+            $this->username = $username;
+        }
+        public function getEmail() {
+            return $this->email;
+        }
+        public function setEmail($email) {
+            $this->email = $email;
+        }
+        public function getHash() {
+            return $this->hash;
+        }
+        public function setHash($hash) {
+            $this->hash = $hash;
+        }
+        public function getName() {
+            return $this->name;
+        }
+        public function setName($name) {
+            $this->name = $name;
+        }
+    }
