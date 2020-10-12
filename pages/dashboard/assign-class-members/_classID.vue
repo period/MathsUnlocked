8bbd1b544bcb91a7d3ef415577ed718a99a7e9fc
Thomas <chearful@chearfulcraft.org>
2020-10-12 11:43:55 +0100

    implement existing class member detection

--- pages/dashboard/assign-class-members/_classID.vue
+++ pages/dashboard/assign-class-members/_classID.vue
@@ -12,7 +12,7 @@
         <b-col lg="4">
             <b-card title="Teachers">
                 <p v-if="this.teachers.length == 0">Your school has no teachers</p>
-                <draggable v-model="teachers" group="members" @start="drag=true" @end="drag=false">
+                <draggable v-model="teachers" group="members" @start="drag=true" @end="drag=false" :move="handleMove">
                     <div v-for="teacher in teachers" :key="teacher.id">{{teacher.name}}</div>
                 </draggable>
             </b-card>
@@ -20,7 +20,7 @@
         <b-col lg="4">
             <b-card title="Students">
                 <p v-if="this.students.length == 0">Your school has no students</p>
-                <draggable v-model="students" group="members" @start="drag=true" @end="drag=false">
+                <draggable v-model="students" group="members" @start="drag=true" @end="drag=false" :move="handleMove">
                     <div v-for="student in students" :key="student.id">{{student.name}}</div>
                 </draggable>
             </b-card>
@@ -28,7 +28,7 @@
         <b-col lg="4">
             <b-card title="Class Members">
                 <p v-if="this.classMembers.length == 0">This class has no members</p>
-                <draggable v-model="classMembers" group="members" @start="drag=true" @end="drag=false">
+                <draggable v-model="classMembers" group="members" @start="drag=true" @end="drag=false" :move="handleMove">
                     <div v-for="member in classMembers" :key="member.id">{{member.name}}</div>
                 </draggable>
             </b-card>
@@ -51,7 +51,8 @@ export default {
             className: "",
             students: [],
             teachers: [],
-            classMembers: []
+            classMembers: [],
+            classLoading: false
         }
     },
     mounted() {
@@ -59,12 +60,50 @@ export default {
         this.loadTeachers();
     },
     methods: {
+        handleMove(evt) {
+            console.log(evt);
+        }
+        async loadClass() {
+            if(this.classLoading == true) return;
+            this.classLoading = true;
+
+            await this.$axios.$get("https://mathsunlockedapi.thomas.gg/class/" + this.classID, {
+                headers: {"Authorization": localStorage.getItem("authorization")}
+            })
+            .then((res) => {
+                let studentIDs = [];
+                let teacherIDs = [];
+                for(var i = 0; i < res.students.length; i++) {
+                    res.students[i].type == "student";
+                    this.classMembers.push(res.students[i]);
+                    this.studentIDs.push(res.students[i].id);
+                }
+                for(var i = 0; i < res.teachers.length; i++) {
+                    res.teachers[i].type == "teacher";
+                    this.classMembers.push(res.teachers[i]);
+                    this.teacherIDs.push(res.teachers[i].id);
+                }
+
+                this.students = this.students.filter((student) => {
+                    return this.studentIDs.includes(student.id) == false;
+                })
+                this.teachers = this.teachers.filter((teacher) => {
+                    return this.teacherIDs.includes(teacher.id) == false;
+                })
+            })
+            .catch((e) => {
+            })
+        }
         async loadStudents() {
             await this.$axios.$get("https://mathsunlockedapi.thomas.gg/school/" + localStorage.getItem("schoolID") + "/students", {
                 headers: {"Authorization": localStorage.getItem("authorization")}
             })
             .then((res) => {
-                this.students = res;
+                for(var i = 0; i < res.length; i++) {
+                    res[i].type == "student";
+                    this.students.push(res[i]);
+                }
+                this.loadClass();
             })
             .catch((e) => {
             })
@@ -74,7 +113,11 @@ export default {
                 headers: {"Authorization": localStorage.getItem("authorization")}
             })
             .then((res) => {
-                this.teachers = res;
+                for(var i = 0; i < res.length; i++) {
+                    res[i].type == "teacher";
+                    this.teachers.push(res[i]);
+                }
+                this.loadClass();
             })
             .catch((e) => {
             })
